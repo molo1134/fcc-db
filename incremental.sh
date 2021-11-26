1f3416990da5a8354641d18a7a81eff1d70a103a
molo1134 <molo1134@gmx.com>
2021-11-25 21:02:20 -0500

    fix issues from shellcheck

--- incremental.sh
+++ incremental.sh
@@ -7,25 +7,25 @@ DATE=$(date "+%Y-%m-%d_%H:%M")
 LOGFILE=log/${DATE}.log
 mkdir -p log
 
-echo "incremental.sh start" >> $LOGFILE
+echo "incremental.sh start" >> "$LOGFILE"
 
 if [ -e $BASEDIR/db.lock ]; then
-	echo "db locked, exiting" >> $LOGFILE
+	echo "db locked, exiting" >> "$LOGFILE"
 	exit 1
 fi
 
-echo "locking DB..." >> $LOGFILE
+echo "locking DB..." >> "$LOGFILE"
 touch $BASEDIR/db.lock
 
-echo "running uls-fetch..." >> $LOGFILE
-./uls-fetch.sh -b `realpath $BASEDIR` -m >> $LOGFILE
+echo "running uls-fetch..." >> "$LOGFILE"
+./uls-fetch.sh -b $(realpath "$BASEDIR") -m >> "$LOGFILE"
 
 LASTWL=$(cat $BASEDIR/last_weekly_l)
 LASTWA=$(cat $BASEDIR/last_weekly_a)
 
-if [ $BASEDIR/weekly_l.zip -nt $LASTWL -o \
-		$BASEDIR/weekly_a.zip -nt $LASTWA ]; then
-	echo "execing load.sh..." >> $LOGFILE
+if [ "$BASEDIR/weekly_l.zip" -nt "$LASTWL" -o \
+		$BASEDIR/weekly_a.zip -nt "$LASTWA" ]; then
+	echo "execing load.sh..." >> "$LOGFILE"
 	exec load.sh
 fi
 
@@ -35,24 +35,24 @@ LASTA=$(cat $BASEDIR/last_import_a)
 # licenses start from sunday
 for d in sun mon tue wed thu fri sat ; do 
 	DAILYL="$BASEDIR/daily_l_$d.zip"
-	if [ $DAILYL -nt $LASTL ]; then
-		echo ./import.pl -c conf-lic.ini $DAILYL >> $LOGFILE
-		./import.pl -c conf-lic.ini $DAILYL
-		echo $DAILYL > $BASEDIR/last_import_l
+	if [ "$DAILYL" -nt "$LASTL" ]; then
+		echo ./import.pl -c conf-lic.ini "$DAILYL" >> "$LOGFILE"
+		./import.pl -c conf-lic.ini "$DAILYL"
+		echo "$DAILYL" > $BASEDIR/last_import_l
 	fi
 done
 
 # applications start from saturday
 for d in sat sun mon tue wed thu fri ; do 
 	DAILYA="$BASEDIR/daily_a_$d.zip"
-	if [ $DAILYA -nt $LASTA ]; then
-		echo ./import.pl -c conf-app.ini $DAILYA >> $LOGFILE
-		./import.pl -c conf-app.ini $DAILYA
-		echo $DAILYA > $BASEDIR/last_import_a
+	if [ "$DAILYA" -nt "$LASTA" ]; then
+		echo ./import.pl -c conf-app.ini "$DAILYA" >> "$LOGFILE"
+		./import.pl -c conf-app.ini "$DAILYA"
+		echo "$DAILYA" > $BASEDIR/last_import_a
 	fi
 done
 
-echo "removing db lock.." >> $LOGFILE
+echo "removing db lock.." >> "$LOGFILE"
 rm -f $BASEDIR/db.lock
 
-echo "incremental.sh done" >> $LOGFILE
+echo "incremental.sh done" >> "$LOGFILE"
--- load.sh
+++ load.sh
@@ -8,26 +8,26 @@ DATE=$(date "+%Y-%m-%d_%H:%M")
 LOGFILE=log/${DATE}.log
 mkdir -p log
 
-echo "load.sh start" >> $LOGFILE
+echo "load.sh start" >> "$LOGFILE"
 
-echo "Removing old DB files.." >> $LOGFILE
+echo "Removing old DB files.." >> "$LOGFILE"
 
 rm -f uls-app.sqlite
 rm -f uls-lic.sqlite
 
 mkdir -p $BASEDIR
-echo "locking DB..." >> $LOGFILE
+echo "locking DB..." >> "$LOGFILE"
 touch $BASEDIR/db.lock
 
-echo "running uls-fetch..." >> $LOGFILE
-./uls-fetch.sh -b `realpath $BASEDIR` -m
+echo "running uls-fetch..." >> "$LOGFILE"
+./uls-fetch.sh -b $(realpath $BASEDIR) -m
 
-echo ./import.pl -n -c conf-lic.ini $BASEDIR/weekly_l.zip >> $LOGFILE
+echo ./import.pl -n -c conf-lic.ini $BASEDIR/weekly_l.zip >> "$LOGFILE"
 ./import.pl -n -c conf-lic.ini $BASEDIR/weekly_l.zip
 echo $BASEDIR/weekly_l.zip > $BASEDIR/last_import_l
 echo $BASEDIR/weekly_l.zip > $BASEDIR/last_weekly_l
 
-echo ./import.pl -n -c conf-app.ini $BASEDIR/weekly_a.zip >> $LOGFILE
+echo ./import.pl -n -c conf-app.ini $BASEDIR/weekly_a.zip >> "$LOGFILE"
 ./import.pl -n -c conf-app.ini $BASEDIR/weekly_a.zip
 echo $BASEDIR/weekly_a.zip > $BASEDIR/last_import_a
 echo $BASEDIR/weekly_a.zip > $BASEDIR/last_weekly_a
@@ -36,7 +36,7 @@ echo $BASEDIR/weekly_a.zip > $BASEDIR/last_weekly_a
 for d in sun mon tue wed thu fri sat ; do 
 	DAILYL="$BASEDIR/daily_l_$d.zip"
 	if [ $DAILYL -nt $BASEDIR/weekly_l.zip ]; then
-		echo ./import.pl -c conf-lic.ini $DAILYL >> $LOGFILE
+		echo ./import.pl -c conf-lic.ini $DAILYL >> "$LOGFILE"
 		./import.pl -c conf-lic.ini $DAILYL
 		echo $DAILYL > $BASEDIR/last_import_l
 	fi
@@ -46,13 +46,13 @@ done
 for d in sat sun mon tue wed thu fri ; do 
 	DAILYA="$BASEDIR/daily_a_$d.zip"
 	if [ $DAILYA -nt $BASEDIR/weekly_a.zip ]; then
-		echo ./import.pl -c conf-app.ini $DAILYA >> $LOGFILE
+		echo ./import.pl -c conf-app.ini $DAILYA >> "$LOGFILE"
 		./import.pl -c conf-app.ini $DAILYA
 		echo $DAILYA > $BASEDIR/last_import_a
 	fi
 done
 
-echo "removing db lock.." >> $LOGFILE
+echo "removing db lock.." >> "$LOGFILE"
 rm $BASEDIR/db.lock
 
-echo "load.sh done" >> $LOGFILE
+echo "load.sh done" >> "$LOGFILE"
