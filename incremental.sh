7043eb1cd206be440979480eea67807367c33a8e
molo1134 <molo1134@gmx.com>
2021-12-11 09:24:13 -0500

    capture stderr; fix a missing redirect

--- incremental.sh
+++ incremental.sh
@@ -18,7 +18,7 @@ echo "locking DB..." >> "$LOGFILE"
 touch $BASEDIR/db.lock
 
 echo "running uls-fetch..." >> "$LOGFILE"
-./uls-fetch.sh -b $(realpath "$BASEDIR") -m >> "$LOGFILE"
+./uls-fetch.sh -b $(realpath "$BASEDIR") -m >> "$LOGFILE" 2>&1
 
 LASTWL=$BASEDIR/last_weekly_l
 LASTWA=$BASEDIR/last_weekly_a
@@ -39,7 +39,7 @@ for d in sun mon tue wed thu fri sat ; do
 	DAILYL="$BASEDIR/daily_l_$d.zip"
 	if [ "$DAILYL" -nt "$LASTL" ]; then
 		echo ./import.pl -c conf-lic.ini "$DAILYL" >> "$LOGFILE"
-		./import.pl -c conf-lic.ini "$DAILYL" >> "$LOGFILE"
+		./import.pl -c conf-lic.ini "$DAILYL" >> "$LOGFILE" 2>&1
 		echo "$DAILYL" > $BASEDIR/last_import_l
 	fi
 done
@@ -49,7 +49,7 @@ for d in sat sun mon tue wed thu fri ; do
 	DAILYA="$BASEDIR/daily_a_$d.zip"
 	if [ "$DAILYA" -nt "$LASTA" ]; then
 		echo ./import.pl -c conf-app.ini "$DAILYA" >> "$LOGFILE"
-		./import.pl -c conf-app.ini "$DAILYA" >> "$LOGFILE"
+		./import.pl -c conf-app.ini "$DAILYA" >> "$LOGFILE" 2>&1
 		echo "$DAILYA" > $BASEDIR/last_import_a
 	fi
 done
--- load.sh
+++ load.sh
@@ -20,15 +20,15 @@ echo "locking DB..." >> "$LOGFILE"
 touch $BASEDIR/db.lock
 
 echo "running uls-fetch..." >> "$LOGFILE"
-./uls-fetch.sh -b $(realpath $BASEDIR) -m
+./uls-fetch.sh -b $(realpath $BASEDIR) -m >> "$LOGFILE" 2>&1
 
 echo ./import.pl -n -c conf-lic.ini $BASEDIR/weekly_l.zip >> "$LOGFILE"
-./import.pl -n -c conf-lic.ini $BASEDIR/weekly_l.zip >> "$LOGFILE"
+./import.pl -n -c conf-lic.ini $BASEDIR/weekly_l.zip >> "$LOGFILE" 2>&1
 echo $BASEDIR/weekly_l.zip > $BASEDIR/last_import_l
 echo $BASEDIR/weekly_l.zip > $BASEDIR/last_weekly_l
 
 echo ./import.pl -n -c conf-app.ini $BASEDIR/weekly_a.zip >> "$LOGFILE"
-./import.pl -n -c conf-app.ini $BASEDIR/weekly_a.zip >> "$LOGFILE"
+./import.pl -n -c conf-app.ini $BASEDIR/weekly_a.zip >> "$LOGFILE" 2>&1
 echo $BASEDIR/weekly_a.zip > $BASEDIR/last_import_a
 echo $BASEDIR/weekly_a.zip > $BASEDIR/last_weekly_a
 
@@ -37,7 +37,7 @@ for d in sun mon tue wed thu fri sat ; do
 	DAILYL="$BASEDIR/daily_l_$d.zip"
 	if [ $DAILYL -nt $BASEDIR/weekly_l.zip ]; then
 		echo ./import.pl -c conf-lic.ini $DAILYL >> "$LOGFILE"
-		./import.pl -c conf-lic.ini $DAILYL >> "$LOGFILE"
+		./import.pl -c conf-lic.ini $DAILYL >> "$LOGFILE" 2>&1
 		echo $DAILYL > $BASEDIR/last_import_l
 	fi
 done
@@ -47,7 +47,7 @@ for d in sat sun mon tue wed thu fri ; do
 	DAILYA="$BASEDIR/daily_a_$d.zip"
 	if [ $DAILYA -nt $BASEDIR/weekly_a.zip ]; then
 		echo ./import.pl -c conf-app.ini $DAILYA >> "$LOGFILE"
-		./import.pl -c conf-app.ini $DAILYA >> "$LOGFILE"
+		./import.pl -c conf-app.ini $DAILYA >> "$LOGFILE" 2>&1
 		echo $DAILYA > $BASEDIR/last_import_a
 	fi
 done
